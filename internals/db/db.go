92727f5c3deb8846e49f28648ce105edcaea3854
Crampustallin <muha.br.ba@gmail.com>
2025-01-24 17:13:27 +0500

    connect

--- internals/db/db.go
+++ internals/db/db.go
@@ -1,12 +1,43 @@
 package db
 
+import (
+	"github.com/Crampustallin/ilyaGG/internals/models"
+	"github.com/jmoiron/sqlx"
+)
+
 type DataBase struct {
+	pg *sqlx.DB
 }
 
-func New() *DataBase {
-	return &DataBase{}
+func New(driver, connectionString string) *DataBase {
+	pg, err := sqlx.Connect(driver, connectionString)
+	if err != nil {
+		panic(err)
+	}
+
+	return &DataBase{
+		pg: pg,
+	}
 }
 
-func (db *DataBase) GetUsers() {
+func (db *DataBase) GetUsers() (users []models.User, err error) {
+	query := "SELECT user_id, user_name, user_score FROM users"
+	rows, err := db.pg.Queryx(query)
+	if err != nil {
+		return nil, err
+	}
 
+	defer func() {
+		err = rows.Close()
+	}()
+
+	for rows.Next() {
+		var user models.User
+		if err := rows.StructScan(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
 }
