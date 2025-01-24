62a0f32f36f646970fb3c90cf408872eb983b3f2
Crampustallin <muha.br.ba@gmail.com>
2025-01-24 19:26:23 +0500

    user_games table updated

--- sql/user_games.sql
+++ sql/user_games.sql
@@ -5,7 +5,10 @@ CREATE TABLE user_games
 	user_id int NOT NULL, -- primary key from users
 	game_title text NOT NULL, -- primary key from games
 	game_rating int, 
-	game_review text
+	game_review text,
+	game_status int,
+	add_date date DEFAULT CURRENT_DATE,
+	upd_date date DEFAULT CURRENT_DATE
 );
 
 ALTER TABLE user_games OWNER TO "ilyaGG"
